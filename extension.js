125ab357a2fe90442dfbe7c88f500e0e30ec87c8
fthx <23138504+fthx@users.noreply.github.com>
2025-01-12 16:28:21 +0100

    Syntax style cleanup

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 //    Power Profile Indicator
 //    GNOME Shell extension
-//    @fthx 2024
+//    @fthx 2025
 
 
 import GLib from 'gi://GLib';
@@ -18,36 +18,36 @@ class PowerProfileIndicator extends SystemIndicator {
         this._indicator = this._addIndicator();
 
         this._timeout = GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
-            this._power_profile_toggle = Main.panel.statusArea.quickSettings._powerProfiles.quickSettingsItems[0];
-            if (this._power_profile_toggle) {
-                this._power_profile_toggle._proxy.connectObject('g-properties-changed', this._set_icon.bind(this), this);
-                this._set_icon();
+            this._powerProfileToggle = Main.panel.statusArea.quickSettings._powerProfiles.quickSettingsItems[0];
+            if (this._powerProfileToggle) {
+                this._powerProfileToggle._proxy?.connectObject('g-properties-changed', this._setIcon.bind(this), this);
+                this._setIcon();
             }
 
             return GLib.SOURCE_REMOVE;
         });
     }
 
-    _set_icon() {
-        this._indicator.icon_name = this._power_profile_toggle.icon_name;
+    _setIcon() {
+        this._indicator.icon_name = this._powerProfileToggle.icon_name;
 
-        if (this._active_style_class_name) {
-            this._indicator.remove_style_class_name(this._active_style_class_name);
-        }
-        this._active_style_class_name = this._power_profile_toggle._proxy.ActiveProfile;
-        if (this._active_style_class_name) {
-            this._indicator.add_style_class_name(this._active_style_class_name);
-        }
+        if (this._activeStyleClassName)
+            this._indicator.remove_style_class_name(this._activeStyleClassName);
+        this._activeStyleClassName = this._powerProfileToggle._proxy.ActiveProfile;
+        if (this._activeStyleClassName)
+            this._indicator.add_style_class_name(this._activeStyleClassName);
     }
 
     _destroy() {
         if (this._timeout) {
-            GLib.source_remove(this._timeout);
+            GLib.Source.remove(this._timeout);
             this._timeout = null;
         }
 
-        this._power_profile_toggle._proxy.disconnectObject(this);
-        this._power_profile_toggle = null;
+        if (this._powerProfileToggle) {
+            this._powerProfileToggle._proxy?.disconnectObject(this);
+            this._powerProfileToggle = null;
+        }
 
         this._indicator.destroy();
         this._indicator = null;
